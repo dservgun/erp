e835eafefef2616b08b419962dea639c6c125b32
Dinkar Ganti <dinkar_ganti@hotmail.com>
2014-02-28 08:32:53 -0500

    Exception handling and ui

--- src/server/ErpModel.hs
+++ src/server/ErpModel.hs
@@ -1,23 +1,27 @@
 module ErpModel where
 import Control.Monad.State
 import Control.Monad.Reader
+import Control.Exception
 import qualified Control.Applicative as C
 import qualified Data.Acid as A
 import Data.Acid.Remote
 import Data.SafeCopy
 import Data.Typeable
 import qualified Data.Map as M
+import qualified Data.Set as S
 import qualified Data.Aeson as J
 import qualified Data.Text.Lazy.Encoding as E
 import qualified Data.Text.Lazy as L
+import Data.Dynamic
 import Data.Time.Clock
 import GHC.Generics
 import qualified Login as Lo
 import qualified Company as Co
 
 data ErpModel = ErpModel
-                {company :: Co.Company,
-                 login :: Lo.Login} deriving(Show, Generic, Typeable, Eq, Ord)
+                {partySet :: S.Set Co.Party ,
+                 companySet :: S.Set Co.Company
+                 } deriving(Show, Generic, Typeable, Eq, Ord)
 {-- A given email id can be tied to only a single erp model, 
  though a given model can be associated with multiple email ids--}                 
 data Database = Database ! (M.Map Lo.Login ErpModel)
@@ -33,20 +37,54 @@ $(deriveSafeCopy 0 'base ''ErpModel)
   
 $(A.makeAcidic ''Database [])
 
-data RequestType = Create | Modify | Retrieve | Delete
+data RequestType = Create | Modify | Retrieve | Delete deriving (Show, Generic, Typeable, Eq, Ord)
+type RequestEntity = String
+data Request = Request {rType :: RequestType,
+                        requestEntity :: RequestEntity,
+                        payload :: L.Text} deriving(Show, Generic, Typeable, Eq, Ord)
+data InvalidRequest = InvalidRequest deriving (Show, Generic, Typeable, Eq, Ord)
+data InvalidLogin = InvalidLogin deriving (Show, Generic, Typeable, Eq, Ord)
+data InvalidCategory = InvalidCategory deriving (Show, Generic, Typeable, Eq, Ord)
+instance Exception InvalidCategory
+instance Exception InvalidLogin
+instance Exception InvalidRequest
+instance J.ToJSON InvalidLogin
+instance J.FromJSON InvalidLogin
+instance J.ToJSON RequestType
+instance J.FromJSON RequestType
+instance J.ToJSON Request
+instance J.FromJSON Request
+
 
 initializeDatabase  dbLocation = A.openLocalStateFrom dbLocation $ Database M.empty
 disconnect = A.closeAcidState
 
+pJSON = J.decode . E.encodeUtf8 . L.fromStrict
 updateDatabase acid aMessage = 
     let 
-        object = J.decode $ E.encodeUtf8 $ L.fromStrict aMessage
+        r = J.decode $ E.encodeUtf8 $ L.fromStrict aMessage
     in 
-        case object of
-            Just l@(Lo.Login email verified) -> updateLogin acid l
-            Nothing -> return() 
-    
+        case r of         
+        Just aRequest -> processRequest acid aRequest
+        _ -> throw InvalidRequest
 
-updateLogin acid aLogin = return ()
-updateCategory acid aCategory = return ()
-updateCompany acid aCompany = return ()    
+processRequest acid r@(Request aType entity payload)  = 
+    case entity of
+    "Login" -> updateLogin acid $ L.toStrict payload
+    "Category" -> updateCategory acid $ L.toStrict payload
+    _ -> throw InvalidRequest
+updateLogin acid payload = 
+     let
+        pObject = J.decode $ E.encodeUtf8 $ L.fromStrict payload
+     in 
+        case pObject of
+            Just (Lo.Login name email) -> return ()
+            Nothing -> throw InvalidLogin 
+
+updateCategory acid payload =
+    let 
+        pObject = pJSON payload
+    in
+        case pObject of
+            Just (Co.Category aCat) -> return ()
+            Nothing -> throw InvalidCategory
